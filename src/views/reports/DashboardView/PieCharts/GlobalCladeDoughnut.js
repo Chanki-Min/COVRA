5ff49b8c493a6d78631da7a6db63f030cc3d9575
GreenApple <alscksrl2010@gmail.com>
2020-09-10 11:48:59 +0900

    Make custom tooltip to doughnut graph

--- src/views/reports/DashboardView/PieCharts/GlobalCladeDoughnut.js
+++ src/views/reports/DashboardView/PieCharts/GlobalCladeDoughnut.js
@@ -8,7 +8,6 @@ import {
   CardContent,
   CardHeader,
   Divider,
-  Typography,
   colors,
   makeStyles,
   useTheme
@@ -30,11 +29,11 @@ const GlobalCladeDoughnut = ({className, ...rest}) => {
         data: [5326, 3975, 4775, 18946, 29562, 19464, 3685],
         backgroundColor: [
           colors.green[500],
-          colors.yellow[600],
-          colors.purple[600],
+          colors.yellow[500],
+          colors.purple[500],
           colors.red[500],
-          colors.red[600],
-          colors.red[800],
+          colors.brown[500],
+          colors.teal[500],
           colors.grey[500],
         ],
         borderWidth: 8,
@@ -42,8 +41,11 @@ const GlobalCladeDoughnut = ({className, ...rest}) => {
         hoverBorderColor: colors.common.white
       }
     ],
-    labels: ['S', 'L', 'V', 'G', 'GR', 'GH', 'Other']
-  }
+    labels: ['S', 'L', 'V', 'G', 'GR', 'GH', 'Other'],
+    firstOccurredLoc: ['우한', '베이징', '서울', '도쿄', '테스트', '테스트2', '테스트3'],
+    firstOccurredAt: ['2020-02-13', '2020-02-14', '2020-02-15', '2020-02-16', '2020-02-17', '2020-02-18', '2020-02-19'],
+    mortality: [10.1, 20.1, 30.1, 40.1, 50.1, 60.1, 0],
+   }
 
   const options = {
     animation: {
@@ -66,7 +68,25 @@ const GlobalCladeDoughnut = ({className, ...rest}) => {
       footerFontColor: theme.palette.text.secondary,
       intersect: false,
       mode: 'index',
-      titleFontColor: theme.palette.text.primary
+      titleFontColor: theme.palette.text.primary,
+      //사용자 지정 툴팁을 만든다
+      callbacks: {
+        title: (tooltipItem, data) => {
+          return `Clade information`
+        },
+        label: (tooltipItem, data) => {
+          return `Clade : ${data.labels[tooltipItem.index]}`
+        },
+        afterLabel: (tooltipItem, data) => {
+          const arr = [];
+          arr.push(`Occurrences : ${data.datasets[0].data[tooltipItem.index]}`);
+          arr.push(`Mortality : ${data.mortality[tooltipItem.index]}`);
+          arr.push(`First occurred location : ${data.firstOccurredLoc[tooltipItem.index]}`)
+          arr.push(`First occurred at : ${data.firstOccurredAt[tooltipItem.index]}`)
+          return arr.join('\n');
+        },
+        footer: () => 'data source : GISAID'
+      }
     }
   };
 
