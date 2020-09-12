b75c4031b5a5907f4a8b53023347bd8acffbe946
GreenApple <alscksrl2010@gmail.com>
2020-09-13 00:22:01 +0900

    Make GlobalStatusGraph component to fetch and use backend data, make data processor
    그래프 차트가 백엔드의 정보를 가져오도록 기능추가하고, 백엔드에서 받아온 정보를
    그래프 차트가 이해할 수 있는 데이터로 처리하는 ProcessFetchData.js 모듈 작성함

--- src/views/reports/DashboardView/Graphs/GolbalStatusGraph.js
+++ src/views/reports/DashboardView/Graphs/GolbalStatusGraph.js
@@ -1,26 +1,54 @@
-import React from "react";
-import clsx from "clsx";
-import {Box, Button, Card, CardContent, CardHeader, colors, Divider, Grow, makeStyles, Popper, Typography, useTheme} from '@material-ui/core';
-import {Bar} from "react-chartjs-2";
-import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-import Paper from "@material-ui/core/Paper";
-import ClickAwayListener from "@material-ui/core/ClickAwayListener";
-import MenuList from "@material-ui/core/MenuList";
-import MenuItem from "@material-ui/core/MenuItem";
+import React, { useEffect } from 'react';
+import clsx from 'clsx';
+import {
+  Box, Button, Card, CardContent, CardHeader, colors, Divider, Grow, makeStyles, Popper, Typography, useTheme
+} from '@material-ui/core';
+import { Bar } from 'react-chartjs-2';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import Paper from '@material-ui/core/Paper';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import MenuList from '@material-ui/core/MenuList';
+import MenuItem from '@material-ui/core/MenuItem';
+import _ from 'lodash';
+import processFetchData from './ProcessFetchData';
 
-const useStyles = makeStyles(() =>
-  ({
-    root: {}
-  }));
+const useStyles = makeStyles(() => ({
+  root: {}
+}));
 
-const GlobalStatusGraph = ({className, ...rest}) => {
+const GlobalStatusGraph = ({ className, ...rest }) => {
   const graphViewOptions = ['By days', 'By week', 'By month'];
   const anchorRef = React.useRef(null);
-  const [open, setOpen] = React.useState(false);
-  const [selectedIndex, setSelectedIndex] = React.useState(0);
-
+  const chartRef = React.useRef(null);
   const classes = useStyles();
   const theme = useTheme();
+
+  const [open, setOpen] = React.useState(false);
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+  const [dataList, setDataList] = React.useState([]);
+  let dataListClone = _.cloneDeep(dataList);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/nationalStatus?dayQ=15&weekQ=15&monthQ=8&nation=global');
+      const json = await response.json();
+      const processed = await processFetchData(json);
+      setDataList(processed);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  useEffect(() => {
+    console.log('updating data');
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log('applying data change');
+    // state 를 바로 chart.js 에 넘기면 문제가 발생하므로 딥 클론을 생성한다.
+    dataListClone = _.cloneDeep(dataList);
+  }, [dataList]);
 
   const handleMenuItemClick = (event, index) => {
     setSelectedIndex(index);
@@ -36,73 +64,13 @@ const GlobalStatusGraph = ({className, ...rest}) => {
       return;
     }
     setOpen(false);
-  }
+  };
 
-  //그래프 데이터
-  const dataList = [
-    {
-      //By day
-      datasets: [
-        {
-          backgroundColor: [
-            colors.indigo[500],
-            colors.indigo[500],
-            colors.indigo[500],
-            colors.indigo[500],
-            colors.green[500],
-            colors.green[500],
-            colors.green[500],
-            ],
-          data: [216452, 266120, 271234, 242321, 235421, 267432, 254231],
-          label: 'Confirmed'
-        },
-        {
-          backgroundColor: colors.red[600],
-          data: [4497, 5632, 6434, 4753, 4214, 6432, 7214],
-          label: 'Death'
-        }
-      ],
-      labels: ['8/31', '9/1', '9/2', '9/3', '9/4', '9/5', '9,6']
-    },
-    {
-      //By week
-      datasets: [
-        {
-          backgroundColor: colors.indigo[500],
-          data: [32, 266120, 271234, 242321, 235421, 267432, 254231],
-          label: 'Confirmed'
-        },
-        {
-          backgroundColor: colors.red[600],
-          data: [4497, 5632, 6434, 4753, 4214, 6432, 7214],
-          label: 'Death'
-        }
-      ],
-      labels: ['3 weeks ago', '2 weeks ago', '1 weeks ago', 'this week', '1 week after', '2 week after', '3 week after']
-    },
-    {
-      //By month
-      datasets: [
-        {
-          backgroundColor: colors.indigo[500],
-          data: [1216452, 132, 1271234, 1242321, 1235421, 1267432, 1254231],
-          label: 'Confirmed'
-        },
-        {
-          backgroundColor: colors.red[600],
-          data: [24497, 15632, 16434, 14753, 14214, 16432, 17214],
-          label: 'Death'
-        }
-      ],
-      labels: ['3m', '2m', '1m', 'm', '11m', '22m', '33m']
-    }
-  ];
-
-  //그래프 표시 옵션
+  // 그래프 표시 옵션
   const options = {
     animationEnabled: true,
     cornerRadius: 0,
-    layout: {padding: 0},
+    layout: { padding: 0 },
     legend: {
       display: true,
       position: 'bottom',
@@ -155,11 +123,11 @@ const GlobalStatusGraph = ({className, ...rest}) => {
       {...rest}
     >
       <CardHeader
-        title='Global Status & Prediction'
+        title="Global Status & Prediction"
         action={(
           <Box ref={anchorRef}>
             <Button
-              endIcon={<ArrowDropDownIcon/>}
+              endIcon={<ArrowDropDownIcon />}
               size="small"
               variant="text"
               aria-controls={open ? 'split-button-menu' : undefined}
@@ -171,7 +139,7 @@ const GlobalStatusGraph = ({className, ...rest}) => {
               {graphViewOptions[selectedIndex]}
             </Button>
             <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal={false}>
-              {({TransitionProps, placement}) => (
+              {({ TransitionProps, placement }) => (
                 <Grow
                   {...TransitionProps}
                   style={{
@@ -200,33 +168,33 @@ const GlobalStatusGraph = ({className, ...rest}) => {
           </Box>
         )}
       />
-      <Divider/>
+      <Divider />
       <CardContent>
         <Box
           height={400}
           position="relative"
         >
           <Bar
-            data={dataList[selectedIndex]}
+            data={dataListClone[selectedIndex]}
             options={options}
+            ref={chartRef}
           />
         </Box>
       </CardContent>
-      <Divider/>
+      <Divider />
       <Box
-      m={2}
+        m={2}
       >
         <Typography
-          color='textPrimary'
-          variant='h6'
+          color="textPrimary"
+          variant="h6"
         >
           목차 클릭시 상세보기 가능합니다. 오늘 이후의 데이터는 추정치입니다.
         </Typography>
       </Box>
 
-
     </Card>
-  )
+  );
 };
 
 export default GlobalStatusGraph;
